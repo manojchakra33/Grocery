34d74b249338df729a85f29e9d7280aa76ad3f88
manojchakra33 <57582321+manojchakra33@users.noreply.github.com>
2022-11-25 16:16:33 +0530

    Update OrdersRepository.java

--- Grocery1/src/main/demo/OrdersRepository.java
+++ Grocery1/src/main/demo/OrdersRepository.java
@@ -3,5 +3,7 @@ package com.example.demo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrdersRepository extends JpaRepository<Orders, Integer>{
+  
+  public List<Orders> findByuser(User user);
 
 }
