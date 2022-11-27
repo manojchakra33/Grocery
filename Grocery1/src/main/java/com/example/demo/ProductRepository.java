2aa3d61bb964a8f411236ef8762e0bede457e6f1
manojchakra33 <57582321+manojchakra33@users.noreply.github.com>
2022-11-27 13:35:10 +0530

    Add files via upload
    spring boot updated

---
+++ Grocery1/src/main/java/com/example/demo/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+	
+	public List<Product> findAllBycategory(Category category);
+	
+	
+
+}
